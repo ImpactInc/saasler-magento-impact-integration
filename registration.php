21ee2b382dc9469582e51b8cb20ab047f5431144
Breba313 <Brebatista@gmail.com>
2021-05-10 16:57:18 -0500

    Update naming convention

--- registration.php
+++ registration.php
@@ -9,6 +9,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'impact_integration',
+    'Impact_Integration',
     __DIR__
 );
