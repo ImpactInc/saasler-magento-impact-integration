4fed3251efc5f9b770c80f5efb7903ecfde92233
PedroIlustre <pedro.ilustre.web@gmail.com>
2022-10-07 17:10:11 -0300

    feat: change InstallData diretory. Use data patches approach in module's Setup/Patch/Data dir

--- Setup/Patch/Data/Uninstall.php
+++ Setup/Patch/Data/Uninstall.php
@@ -6,7 +6,7 @@
 * @copyright   Copyright (c) 2021 Impact. (https://impact.com)
 */
 
-namespace Impact\Integration\Setup;
+namespace Impact\Integration\Setup\Patch\Data;
 
 use Impact\Integration\Service\ImpactApiService; 
 use Magento\Integration\Api\IntegrationServiceInterface;
