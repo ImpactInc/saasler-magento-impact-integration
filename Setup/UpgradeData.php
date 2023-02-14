73dfceff2fb83b1ce4e61fc11b58eb95a2cdc529
jsalguero94 <jsalguero94@gmail.com>
2023-02-14 17:31:39 -0500

    IMA-1179: [Magento] Impact Integration is not available to activate (#44)
    Tasks: [IMA-1179](https://impact.atlassian.net/browse/IMA-1179)

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Impact: Partnership Cloud for Magento
+*
+* @package     Impact_Integration
+* @copyright   Copyright (c) 2021 Impact. (https://impact.com)
+*/
+
+
+namespace Impact\Integration\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+//use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Integration\Model\ConfigBasedIntegrationManager;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var ConfigBasedIntegrationManager
+     */
+
+    private $integrationManager;
+
+    /**
+     * @param ConfigBasedIntegrationManager $integrationManager
+     */
+
+    public function __construct(ConfigBasedIntegrationManager $integrationManager)
+    {
+        $this->integrationManager = $integrationManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->integrationManager->processIntegrationConfig(['ImpactIntegration']);
+    }
+}
