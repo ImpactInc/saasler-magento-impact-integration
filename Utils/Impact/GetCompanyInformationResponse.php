8c10e2034a2119b02a0f556a08029d60df51526a
jsalguero94 <jsalguero94@gmail.com>
2023-02-28 15:52:01 -0500

    feature: create utils Impact to handle credentials validation

---
+++ Utils/Impact/GetCompanyInformationResponse.php
@@ -0,0 +1,71 @@
+<?php
+/**
+* Impact: Partnership Cloud for Magento
+*
+* @package     Impact_Integration
+* @copyright   Copyright (c) 2021 Impact. (https://impact.com)
+*/
+
+namespace Impact\Integration\Utils\Impact;
+
+/**
+ * Class GetCompanyInformationResponse
+ *
+ * @package Impact\Integration\Utils\Impact
+ */
+class GetCompanyInformationResponse
+{
+    /**
+     * @var code
+     */
+    private $code;
+
+    /**
+     * @var data
+     */
+    private $data;
+
+    /**
+     * GetCompanyInformationResponse constructor
+     *
+     * @param mixed $accountSid
+     */
+    public function __construct($response)
+    {
+        $this->code = $response->getStatusCode();
+        if (!$this->failed()) {
+            $this->parseXMLResponse($response->getBody());
+        }
+    }
+
+    /**
+     *  Failed function
+     *  @return bool
+     */
+    public function failed()
+    {
+        return $this->code >= 400;
+    }
+
+    /**
+     *  getData function
+     *  @return json_encode
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     *  parseXMLResponse function
+     *  @return void
+     */
+    private function parseXMLResponse($body)
+    {
+        $xmlObject = simplexml_load_string($body);
+        $root = $xmlObject->getName();
+        $jsonString = json_encode($xmlObject);
+        $jsonString = json_encode([$root => json_decode($jsonString)]);
+        $this->data = json_decode($jsonString, true);
+    }
+}
---
+++ Utils/Impact/ImpactHttpClient.php
@@ -0,0 +1,60 @@
+<?php
+/**
+* Impact: Partnership Cloud for Magento
+*
+* @package     Impact_Integration
+* @copyright   Copyright (c) 2021 Impact. (https://impact.com)
+*/
+
+namespace Impact\Integration\Utils\Impact;
+
+use Impact\Integration\Utils\Impact\GetCompanyInformationResponse;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ClientException;
+
+/**
+ * Class ImpactHttpClient
+ *
+ * @package Impact\Integration\Utils\Impact
+ */
+class ImpactHttpClient
+{
+    /**
+     * Get company information
+     *
+     * @param String $accountSid
+     * @param String $authToken
+     * @return mixed
+     * GetCompanyInformationResponse or false
+     */
+    public static function getCompanyInformation($accountSid, $authToken)
+    {
+        $response = [];
+        $endpoint = "https://api.impact.com/";
+        // New GuzzleHttp Client
+        $client = new Client([
+            'base_uri' => $endpoint,
+            'timeout'  => 5.0,
+        ]);
+
+        try {
+            // Authorization
+            $headers = ['Authorization' => "Basic " . base64_encode($accountSid . ":" . $authToken)];
+            // Request api
+            $response = $client->request('GET', "/Advertisers/$accountSid/CompanyInformation", ['headers' => $headers]);
+
+        } catch(ClientException $exception){
+            //exception is thrown for 400 level errors
+            $response = $exception->getResponse();
+
+        } catch (ConnectException $exception) {
+            //exception is thrown in the event of a networking error.
+            \Magento\Framework\App\ObjectManager::getInstance()->get('Psr\Log\LoggerInterface')->info($exception->getMessage());
+            return false;
+        }
+
+        return new GetCompanyInformationResponse($response);
+    }
+}
